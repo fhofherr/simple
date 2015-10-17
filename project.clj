b6afa45042bb92eba8d66154416974fb94050f0a
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2015-10-17 16:19:25 +0200

    Add codox plugin.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject simple "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A very simple CI server for individual projects."
   :url "https://github.com/fhofherr/simple"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
@@ -10,6 +10,12 @@
   :target-path "target/%s"
   :test-selectors {:unit (complement :integration)
                    :integration :integration}
+  :plugins [[codox "0.8.15" :exclusions [[org.clojure/clojure]]]]
+  :codox {:output-dir "target/doc/api"
+          :src-dir-uri "https://github.com/fhofherr/simple/blob/master/"
+          :src-linenum-anchor-prefix "L"
+          :defaults {:doc/format :markdown}
+          :exclude [user]}
   :profiles {:dev {:source-paths ["dev"]
                    :resource-paths ["dev-resources" "test-resources"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]]}
