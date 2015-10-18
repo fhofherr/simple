a32a23942d76b2174782f80bdd979bae0ba12bc2
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2015-10-18 11:31:46 +0200

    Implement clj-io.files/touch and chmod.

--- test/fhofherr/simple/test/project_test.clj
+++ test/fhofherr/simple/test/project_test.clj
@@ -1,17 +1,14 @@
 (ns fhofherr.simple.test.project-test
   (:require [clojure.test :refer :all]
             [fhofherr.clj-io.files :as files]
-            [fhofherr.simple.project :as prj])
-  (:import [java.nio.file Files]
-           [java.nio.file.attribute PosixFilePermission]))
+            [fhofherr.simple.project :as prj]))
 
 (deftest execute-a-shell-script
   (files/with-tmp-dir
     [path]
     (let [script-path (-> "tests/execute-shell-script.sh"
                           (files/copy-resource (.resolve path "execute-shell-script.sh"))
-                          (Files/setPosixFilePermissions
-                            #{PosixFilePermission/OWNER_READ PosixFilePermission/OWNER_EXECUTE})
+                          (files/chmod "rwx")
                           (.getFileName)
                           (str))
           result ((prj/execute script-path) {:project-dir path})]
