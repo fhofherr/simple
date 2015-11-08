e8f16a1822382a4ad49a565ab0d45f5d799367a8
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2015-11-08 15:07:57 +0100

    Let simple exeucte its jobs from the repl.

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,7 @@
 (ns user
   (:require [clojure.tools.namespace.repl :as repl]
-            [clojure.test :as t]))
+            [clojure.test :as t]
+            [fhofherr.simple.engine :as engine]))
 
 (defn- do-run-unit-tests
   []
@@ -17,3 +18,21 @@
 (defn run-integration-tests
   []
   (repl/refresh :after 'user/do-run-integration-tests))
+
+(def simple-engine nil)
+
+(defn start-simple-ci
+  []
+  (when-not simple-engine
+    (alter-var-root #'simple-engine (constantly (engine/load-engine "." "simple.clj")))))
+
+(defn stop-simple-ci
+  []
+  (when simple-engine
+    (alter-var-root #'simple-engine (constantly nil))))
+
+(defn run-simple-test-job
+  []
+  (if simple-engine
+    (engine/start-job! simple-engine 'test-simple-ci)
+    (println "Call 'start-simple-ci' first!")))
