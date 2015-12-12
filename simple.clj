062d21edace20edc509756b1682e8384cc46b77e
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2015-12-12 12:45:50 +0100

    Trigger tests every 5 minutes

--- simple.clj
+++ simple.clj
@@ -4,4 +4,4 @@
   :test (execute "run-tests.sh")
   :triggers [{:type :timer
               :name "test-simple-ci-trigger"
-              :args [:every 1 :minutes]}])
+              :args [:every 5 :minutes]}])
